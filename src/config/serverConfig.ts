bb18097721021b18665447539b61ce5fea0b7d7b
Mohit <mohit.saini@signzy.com>
2024-06-23 13:50:58 +0530

    updated env and mongo

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -1,6 +1,5 @@
 export default {
 	PORT: process.env.PORT || 3000,
 	HOST: process.env.HOST || 'localhost',
-	DB_STRING: process.env.HOST || 'mongodb://127.0.0.1:27017/scrapbazaar',
-	//mongodb+srv://mssaini348:QchIAfsERV9AGanQ@scrapbazaar.7eccopa.mongodb.net/?retryWrites=true&w=majority&appName=ScrapBazaar
+	DB_STRING: process.env.DB_STING || 'mongodb://127.0.0.1:27017/scrapbazaar',
 };
