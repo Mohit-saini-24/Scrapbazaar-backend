eeea7b491543567aff48db6344f59cec33193e19
Mohit <mohit.saini@signzy.com>
2024-06-23 13:15:27 +0530

    update package manager pnpm

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import mongodb from './plugins/mongodb';
 import { healthRoute } from './routes/health.route';
 
 const app = async (fastify: FastifyInstance) => {
-	void fastify.register(mongodb);
+	// void fastify.register(mongodb);
 	void fastify.register(healthRoute);
 };
 export default app;
